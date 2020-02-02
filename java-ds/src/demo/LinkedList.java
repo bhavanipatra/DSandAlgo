5474f141ed56a741745e7d4b0fe4ae855a60cb18
hp <hp@Bhavani-HPLaptop>
2020-02-02 11:40:52 +0530

    Commit.

--- java-ds/src/demo/LinkedList.java
+++ java-ds/src/demo/LinkedList.java
@@ -13,6 +13,7 @@ public class LinkedList {
 	}
 	private Node first;
 	private Node last;
+	private int size = 0;
 	
 	public void addFirst(int item) {
 		Node node = new Node(item);
@@ -23,6 +24,7 @@ public class LinkedList {
 			node.next = first;
 			first = node;
 		}
+		size++;
 		
 	}
 	
@@ -35,6 +37,7 @@ public class LinkedList {
 			last.next = node;
 			last = last.next;
 		}
+		size++;
 	}
 	
 	public void deleteFirst() {
@@ -46,6 +49,7 @@ public class LinkedList {
 			first.next = null;
 			first = node;
 		}
+		size--;
 	}
 	
 	public void deleteLast() {
@@ -60,6 +64,7 @@ public class LinkedList {
 		}
 		cNode.next = null;
 		last = cNode;
+		size--;
 	}
 	
 	public boolean contains(int item) {
@@ -114,6 +119,57 @@ public class LinkedList {
 		}
 		last = first;
 		first = previous;
+	}
+	
+	public int getKthFromTheEnd(int k) {
+		if(k <= 0) throw new NoSuchElementException();
+		if(k > size) throw new IllegalArgumentException();
+		int gap = (k-1);
+		Node pointer1 = first;
+		Node pointer2 = first;
+		while(gap != 0) {
+			pointer2 = pointer2.next;
+			gap--;
+		}
+		while(pointer2.next != null) {
+			pointer1 = pointer1.next;
+			pointer2 = pointer2.next;
+		}
+		return pointer1.value;
+	}
+
+	//Exercise-1
+	public void printMiddle() {
+		if(first == null) throw new IllegalStateException();
+		Node ahead = first;
+		Node behind = first;
+		int flag =0;
+		while(ahead.next != null) {
+			ahead = ahead.next.next;
+			behind = behind.next;
+			if(ahead.next != null && ahead.next.next == null) {
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 1) System.out.println(behind.value+", "+behind.next.value);
+		else System.out.println(behind.value);
+	}
+	
+	//Exercise-2
+	public void hasLoop() {
+		Node slow;
+		Node fast;
+		slow = fast = first;
+		while(slow != null && fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow == fast) {
+				System.out.println("has Loop");
+				return;
+			}
+			System.out.println("No Loop");
+		}
 	}
 }
 
--- java-ds/src/demo/Main.java
+++ java-ds/src/demo/Main.java
@@ -24,12 +24,17 @@ public class Main {
 //		 
 
 		// Testing of LinkedList
-//		LinkedList list = new LinkedList();
+		LinkedList list = new LinkedList();
 //		list.addLast(1);
 //		list.addLast(2);
 //		list.addLast(3);
+//		list.addLast(4);
+//		list.addLast(5);
+//		list.addLast(6);
 //		list.reverse();
 //		System.out.println(list.toArray());
+		list.printMiddle();
+//		System.out.println(list.getKthFromTheEnd(1));
 		
 	}
 }
